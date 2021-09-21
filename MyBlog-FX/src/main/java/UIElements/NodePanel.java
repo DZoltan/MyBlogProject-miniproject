675967087f7627a4c02b28ec01407b9bbe90929c
DZoltan <dobos.zoltan.esp@gmail.com>
2021-09-21 16:50:20 +0200

    delete és expand gombok hozzáadva a panelhez

--- MyBlog-FX/src/main/java/UIElements/NodePanel.java
+++ MyBlog-FX/src/main/java/UIElements/NodePanel.java
@@ -2,21 +2,32 @@ package UIElements;
 
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.geometry.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class NodePanel extends StackPane {
     Label title = new Label("Teszt");
     Label description = new Label();
-    public NodePanel(){
+    Button delete = new Button();
+    Button expand = new Button();
+
+    public NodePanel() throws IOException {
         this.setBorder(new Border(new BorderStroke(Color.BLACK,
                 BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
         this.setPrefSize(430,200);
-        this.setPadding(new Insets(10));
+        this.setPadding(new Insets(5));
 
         //Title
         title.setFont(new Font("Verdana", 20));
@@ -39,6 +50,22 @@ public class NodePanel extends StackPane {
         description.setPadding(new Insets(15));
         this.getChildren().add(description);
         this.setAlignment(description,Pos.CENTER);
+
+        //delete button
+        //InputStream deleteStream = getClass().getResourceAsStream("/imgs/delete.png");
+        Image deleteImage = new Image(this.getClass().getResourceAsStream("/imgs/delete.png"));
+        delete.setGraphic(new ImageView(deleteImage));
+        this.getChildren().add(delete);
+        this.setAlignment(delete, Pos.TOP_RIGHT);
+
+        //Expand button
+        //InputStream expandStream = getClass().getResourceAsStream("/imgs/expand.png");
+        Image expandImage = new Image(this.getClass().getResourceAsStream("/imgs/expand.png"));
+        expand.setGraphic(new ImageView(expandImage));
+        this.getChildren().add(expand);
+        this.setAlignment(expand, Pos.BOTTOM_RIGHT);
+
+
     }
 
 }
