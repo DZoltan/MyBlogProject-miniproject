63744c5da45898ae1adae479849a762629627ea9
Deák Ruben <rubendeak15@gmail.com>
2021-09-22 14:37:50 +0200

    Postokba vissza lépő gomb aktiválva

--- MyBlog-FX/src/main/java/Controller/CommentController.java
+++ MyBlog-FX/src/main/java/Controller/CommentController.java
@@ -1,9 +1,22 @@
 package Controller;
 
+import UIElements.KommentPanel;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class CommentController {
 
-    public void backToPosts(ActionEvent actionEvent) {
+    @FXML
+    public void backToPosts(ActionEvent actionEvent) throws Exception {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxs/mainWindow.fxml"));
+        Parent root = fxmlLoader.load();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
--- MyBlog-FX/src/main/java/UIElements/KommentPanel.java
+++ MyBlog-FX/src/main/java/UIElements/KommentPanel.java
@@ -1,8 +1,22 @@
 package UIElements;
 
-import javafx.scene.layout.StackPane;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 
-import java.awt.*;
 
 public class KommentPanel extends StackPane {
+    Button back = new Button();
+
+    public KommentPanel() {
+
+        //back
+        Image backImage = new Image(this.getClass().getResourceAsStream("/imgs/back.png"));
+        back.setGraphic(new ImageView(backImage));
+        this.getChildren().add(back);
+        this.setAlignment(back, Pos.BOTTOM_RIGHT);
+
+    }
 }
