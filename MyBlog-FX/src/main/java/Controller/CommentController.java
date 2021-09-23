aed440cb44bb9490a6e90a0147268bf7ce841857
Deák Ruben <rubendeak15@gmail.com>
2021-09-23 11:36:48 +0200

    Egy üzenet megnyitása egy új ablakban történik

--- MyBlog-FX/src/main/java/Controller/CommentController.java
+++ MyBlog-FX/src/main/java/Controller/CommentController.java
@@ -1,22 +1,6 @@
 package Controller;
 
-import UIElements.KommentPanel;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class CommentController {
 
-    @FXML
-    public void backToPosts(ActionEvent actionEvent) throws Exception {
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxs/mainWindow.fxml"));
-        Parent root = fxmlLoader.load();
-        stage.setScene(new Scene(root));
-        stage.show();
-    }
 }
